3110e43a8cd740c65a3f5925e71c82ae67f60d9e
blck-snwmn <whitesnowmancreator@gmail.com>
2023-04-24 02:38:41 +0900

    Change sample

--- cmd/main.go
+++ cmd/main.go
@@ -5,31 +5,100 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 )
 
-type payload struct {
-	X int `json:"x"`
-	Y int `json:"y"`
-	Z int `json:"z"`
+type order struct {
+	ID       string `json:"id"`
+	Item     string `json:"item"`
+	Quantity int    `json:"quantity"`
+	Shipped  bool   `json:"shipped"`
 }
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("recived request")
-		if r.Method == http.MethodPost {
-			fmt.Println("recived post request")
-			decoder := json.NewDecoder(r.Body)
-			var payload payload
-			err := decoder.Decode(&payload)
-			if err != nil {
-				http.Error(w, "Bad request", http.StatusBadRequest)
-				return
-			}
-			fmt.Fprintf(w, "Received payload: %+v", payload)
-		} else {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		}
-	})
+type orderStore struct {
+	orders map[string]order
+	mux    sync.RWMutex
+}
 
+func (s *orderStore) addOrder(o order) {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	s.orders[o.ID] = o
+}
+
+func (s *orderStore) updateOrder(o order) bool {
+	s.mux.Lock()
+	defer s.mux.Unlock()
+	oo, ok := s.orders[o.ID]
+	if !ok {
+		return false
+	}
+	oo.Shipped = o.Shipped
+	s.orders[o.ID] = oo
+	return true
+}
+
+func (s *orderStore) getOrders() []order {
+	s.mux.RLock()
+	defer s.mux.RUnlock()
+	orders := make([]order, 0, len(s.orders))
+	for _, order := range s.orders {
+		orders = append(orders, order)
+	}
+	return orders
+}
+
+var store = &orderStore{orders: map[string]order{}}
+
+func main() {
+	http.HandleFunc("/orders", ordersHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func ordersHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		getOrders(w, r)
+	case "POST":
+		addOrder(w, r)
+	case "PUT":
+		updateOrder(w, r)
+	default:
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+}
+
+func addOrder(w http.ResponseWriter, r *http.Request) {
+	var order order
+	err := json.NewDecoder(r.Body).Decode(&order)
+	if err != nil {
+		fmt.Printf("failed to add order: %v\n", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	store.addOrder(order)
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(order)
+}
+
+func updateOrder(w http.ResponseWriter, r *http.Request) {
+	var order order
+	err := json.NewDecoder(r.Body).Decode(&order)
+	if err != nil {
+		fmt.Printf("failed to update order: %v\n", err)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	ok := store.updateOrder(order)
+	if !ok {
+		fmt.Println("not found")
+		http.NotFound(w, r)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func getOrders(w http.ResponseWriter, r *http.Request) {
+	orders := store.getOrders()
+	json.NewEncoder(w).Encode(orders)
 }
--- k6/script.js
+++ k6/script.js
@@ -1,11 +1,26 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
+import { check, sleep } from 'k6';
 
 export default function () {
-    const payload = JSON.stringify({ x: 12, y: 33, z: 2222 });
+    // 注文登録
+    const order = { id: Math.floor(Math.random() * 100000).toString(), item: 'example', quantity: 1 };
     const headers = { 'Content-Type': 'application/json' };
-    for (let i = 0; i < 100; i++) {
-        const response = http.post('http://localhost:8080', payload, { headers });
-    }
+    const res = http.post('http://localhost:8080/orders', JSON.stringify(order), { headers });
+    check(res, { 'status is 201': (r) => r.status === 201 });
+
+    // 注文状態更新
+    const update = { id: order.id, shipped: true };
+    const res2 = http.put('http://localhost:8080/orders', JSON.stringify(update), { headers });
+    check(res2, { 'status is 200': (r) => r.status === 200 });
+
+    // 注文一覧取得
+    const res3 = http.get('http://localhost:8080/orders');
+    check(res3, { 'status is 200': (r) => r.status === 200 });
+
     sleep(1);
 }
+
+export const options = {
+    vus: 100, // 並列ユーザー数
+    duration: '30s', // テスト実行時間
+};
